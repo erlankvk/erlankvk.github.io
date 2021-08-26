f50fc120f8bfc3bbc8fc9208fe869290e05c0c19
adminlogos <64223666+erlankvk@users.noreply.github.com>
2021-08-26 00:25:54 -0700

    Add files via upload

---
+++ MINEXMR/CPU GPU OPEN CL/WIN/xmrig-6.14.1/start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --opencl --cuda -o pool.minexmr.com:443 -u 44DEfGZKz4JRYLwao9CezzS3jtSXtj4NZaRUhi9u9pbt7J7uUJaJexAHATArfTFHQdVzQV419chVM78WgvLdDV6tS8V2umE -k --tls --rig-id VDSCPU
+pause
